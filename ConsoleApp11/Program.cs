25f5af884c0d022a8e962b17965ee9858fc6b4f2
karinaFlower <72452438+karinaFlower@users.noreply.github.com>
2020-10-15 20:50:10 +0900

    дз

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string x ;
+                x = "Гребенькова";
+                Console.WriteLine($"Результат: {x}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение");
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+            Console.WriteLine("Конец данной программы");
+            Console.Read();
+        }
+    }
+}
